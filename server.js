feca583dcc0a2a0284be13c13b5fb48269b920f9
mohan.kumar <mohan.kumar@inmobi.com>
2014-09-14 20:46:50 +0530

    adding socket-io

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
     res.sendFile('index.html',{ root: '/Applications/MAMP/htdocs/node/chat/node-sample' });
@@ -7,4 +8,15 @@ app.get('/', function(req, res){
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
+});
+
+var index = 0 ;
+
+io.on("connection" ,function(socket){
+    console.log("user connected ");
+    console.log("user "+ ++index);
+    socket.on('disconnect', function(){
+        index--;
+        console.log('user disconnected');
+    });
 });
